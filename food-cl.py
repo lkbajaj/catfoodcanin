dad6a059b630b8cfec2878a803d1439bbe6161d6
lkbajaj <leynabajaj@umass.edu>
2025-06-21 20:10:35 -0400

    initial commit

---
+++ food-cl.py
@@ -0,0 +1,23 @@
+POUNDSTOGRAMS = 1000/2.205 
+
+class CatFood: 
+    def __init__(self,weight,price):
+        self.weight = weight # weight of food in grams
+        self.price = price # total price 
+    
+    def remove_weight(self,weight_removed):
+        self.weight = self.weight-weight_removed
+
+
+class CaninKittenDry3lb(CatFood):
+    def __init__(self,weight,price):
+        weight = 3 # weight in pounds
+        weight = weight * POUNDSTOGRAMS # weight in grams
+        price = 28.49 # subscription price Chewy, Amazon, Site
+        super().__init__(weight,price)
+
+class CaninKittenWet3oz(CatFood):
+    def __init__(self,weight,price):
+        weight = 85 # weight in grams
+        price = 0.77 # price per 3 oz can
+        super().__init__(weight,price)
